993b3b90aa27c86a09982f5c5195a8ea87e99a1b
Anil Desai <anil_desai@thbs.india.com>
2020-01-23 11:44:06 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+//add this file to .gitignore
+
+module.exports ={
+    google: {
+
+        clientID :'720447376174-otbc7ssg4qqnoq6ebhsl9tgvh59bo171.apps.googleusercontent.com',
+        clientSecret:'xb0HkYvNAPE2fz6933VdQrhj'
+    },
+    mongodb:{
+        dbURI:'mongodb+srv://anil:test123@cluster0-gyxrt.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session:{
+        cookieKey:'welcomeanildesai'
+    }
+};
